b5561e8638087c59faa29868a247624f12ef0868
qzhqzh <qin28630707@gmail.com>
2026-07-31 22:30:59 +0800

    test: allow Django test client host

---
+++ backend/tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def allow_django_test_client_host(settings):
+    if "testserver" not in settings.ALLOWED_HOSTS:
+        settings.ALLOWED_HOSTS = [*settings.ALLOWED_HOSTS, "testserver"]
